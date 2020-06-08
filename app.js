040e05c9e09ec2494e59558639325266295df6d1
ntavora <ntavora@devsutd.com>
2020-06-08 11:40:17 -0300

    fix routes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ if ('development' == app.get('env')) {
 // HubExchange Routes
 
 app.get('/login', routes.login);
-app.post('/logout', routes.logout);
+app.get('/logout', routes.logout);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
