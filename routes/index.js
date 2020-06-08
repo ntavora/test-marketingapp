a5b11a9a234c6adb3a2135def7224ca089ad7d57
ntavora <ntavora@devsutd.com>
2020-06-08 11:36:32 -0300

    POC  redirect using query parameter on package url

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,27 @@ const jwtDecode = require('jwt-decode');
 exports.login = (req, res) => {
 
   try {
-    console.log(req.queryparameter);
-    res.redirect('https://mc2h-lnx9y6g8mb6fw8vdx6d5cn8.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id=5cwinm5nfzk8mz6cmu16j1fh&redirect_uri=https://test-marketingapp.herokuapp.com/login');
+    let stateParam = '&state=journey';
+    if (req.query.state !== undefined) {
+        stateParam = `&state=${req.query.state}`;
+    }
+    if (req.query.code === undefined) {
+    res.redirect(`https://mc2h-lnx9y6g8mb6fw8vdx6d5cn8.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id=5cwinm5nfzk8mz6cmu16j1fh&redirect_uri=https://test-marketingapp.herokuapp.com/login${stateParam}`);
+    } else {
+
+      const { state } = req.query;
+      if(state == "automation"){
+         res.redirect('www.devsutd.com');
+      }
+
+      if(state == "journey"){
+        res.redirect('www.google.com');
+     }
+    }
+
+
+
+
   } catch (err) {
     return res.send(200, err);
   }
