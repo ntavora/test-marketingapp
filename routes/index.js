ecd1274e55798f409f3502f4bf5adaac8af9d6a8
ntavora <ntavora@devsutd.com>
2020-06-08 11:28:36 -0300

    Prueba recibir parametros

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const jwtDecode = require('jwt-decode');
 exports.login = (req, res) => {
 
   try {
-    console.log(req);
-    res.redirect('https://mc2h-lnx9y6g8mb6fw8vdx6d5cn8.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id=jdw30r1q35v34m7x1iyqwerx&redirect_uri=https://appsflyer-mc-app-dev.herokuapp.com/home&scope=email_read%20email_write%20email_send&state=mystate');
+    console.log(req.queryparameter);
+    res.redirect('https://mc2h-lnx9y6g8mb6fw8vdx6d5cn8.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id=5cwinm5nfzk8mz6cmu16j1fh&redirect_uri=https://test-marketingapp.herokuapp.com/login');
   } catch (err) {
     return res.send(200, err);
   }
