343db78e763ea31f52464a86c70e8a1591aad47c
ntavora <ntavora@devsutd.com>
2020-06-30 08:44:01 -0300

    test

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const jwtDecode = require('jwt-decode');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr('myTotalySecretKey');
 exports.login = (req, res) => {
 
     try {
@@ -23,6 +25,12 @@ exports.login = (req, res) => {
             };
             var sign = 'v-7JCVJsWAFbQZqIeE2BOW9oXU2NjZTU9kWn5QnxvasiS7W-JRt7XwEbPOH0vJ2wzx33_aPGWKcsxMRGijg9L2Uf425soBc1_iYzCmPYYpmdkhnKQY3SUuJjsaSwki64hEIOEYNSWDqyRJJ15GnOJcW_HhMn-Jhwgynz7aNlaK_nlb9phBU0C0GRabjfcQyifovxbhmaVzc_vR7pVX-lV5-V98Ijldp4GiCKbL_W6OrGGor_GgsuGYnCysWykg2'
             var token = jwt.sign(JSON.stringify(request), sign);
+
+            const encryptedString = cryptr.encrypt(token);
+            const decryptedString = cryptr.decrypt(encryptedString);
+
+            console.log(encryptedString);
+            console.log(decryptedString);
             console.log("decoded " + jwt.decode(token, sign));
             console.log(token);
 
