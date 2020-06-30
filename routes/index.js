28aa56f775b08e43abea0f60afa7a989c4a09669
ntavora <ntavora@devsutd.com>
2020-06-30 08:34:03 -0300

    encrypt

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ exports.login = (req, res) => {
             var token = jwt.sign(JSON.stringify(request), sign);
             console.log("decoded " + jwt.decode(token, sign));
             console.log(token);
-            const hashedPassword = bcrypt.hashSync(token, 'kamicASE');
+            const hashedPassword = bcrypt.hashSync(token, 10);
             console.log("encrypted value " + hashedPassword);
             res.cookie('cookiename', hashedPassword, { maxAge: 900000, httpOnly: true });
             if (state == "automation") {
