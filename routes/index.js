b92c28249e6d2b28f7378be53d35cafc9440446c
ntavora <ntavora@devsutd.com>
2020-06-30 08:29:37 -0300

    add console.log

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ exports.login = (req, res) => {
             };
             var sign = 'v-7JCVJsWAFbQZqIeE2BOW9oXU2NjZTU9kWn5QnxvasiS7W-JRt7XwEbPOH0vJ2wzx33_aPGWKcsxMRGijg9L2Uf425soBc1_iYzCmPYYpmdkhnKQY3SUuJjsaSwki64hEIOEYNSWDqyRJJ15GnOJcW_HhMn-Jhwgynz7aNlaK_nlb9phBU0C0GRabjfcQyifovxbhmaVzc_vR7pVX-lV5-V98Ijldp4GiCKbL_W6OrGGor_GgsuGYnCysWykg2'
             var token = jwt.sign(JSON.parse(request), sign);
-            console.log("decoded ", jwt.decode(token, sign));
+            console.log("decoded " + jwt.decode(token, sign));
             console.log(token);
             const hashedPassword = bcrypt.hashSync(token, 'kamicASE');
             console.log("encrypted value " + hashedPassword);
